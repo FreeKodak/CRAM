62391a254b383708fa22954d9e4fdfedc3a8326e
FreeKodak <daniel_orduz19@outlook.com>
2019-11-12 14:27:24 -0700

    initial commit

---
+++ components/UsersMap.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import MapView from 'react-native-maps';
+
+const usersMap = props => {
+    return (
+        <View style={styles.mapContainer}>
+            <MapView style = {styles.map}
+            region={props.userLocation}>
+                <MapView.Marker coordinate={props.userLocation}/> 
+            </MapView> 
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    mapContainer: {
+        width: '100%',
+        height: 200
+
+    },
+    map: {
+        width: '100%',
+        height: '100%'
+    }
+});
+export default usersMap;
