b0e7d1735214d9e7c0bc7e4038c0d9f4005e3b63
FreeKodak <daniel_orduz19@outlook.com>
2019-11-15 14:28:25 -0700

    logo added
    logo added and colours flipped so we can see the logo

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 "use strict";
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { Button } from "react-native-elements";
 import FetchLocation from "./components/FetchLocation";
 import UsersMap from "./components/UsersMap";
@@ -19,10 +19,18 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.yellowbox} />
+        <View style={styles.yellowbox}>
+          <Image
+            source={require("C:/Users/GEO OIL ENERGY 2016/Documents/Projects/CramApp/branding/logo.png")}
+          />
+        </View>
         <View style={styles.greybox}>
-          <Button style={styles.button} title="Login" />
-          <Button title="Register" />
+          <TouchableOpacity style={styles.loginButton}>
+            <Text>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.registerButton}>
+            <Text>Register</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -40,18 +48,44 @@ const styles = StyleSheet.create({
   yellowbox: {
     height: 300,
     width: 500,
-    backgroundColor: "#FCBA03"
-  },
-
-  greybox: {
-    height: 500,
-    width: 500,
     backgroundColor: "grey",
     alignItems: "center",
     justifyContent: "center"
   },
 
-  button: {
-    backgroundColor: "#FCBA03"
+  greybox: {
+    height: 500,
+    width: 500,
+    backgroundColor: "#FCBA03",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  loginButton: {
+    width: 100,
+    backgroundColor: "grey",
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 25,
+    paddingRight: 25,
+    borderRadius: 10,
+    borderWidth: 1
+  },
+
+  registerButton: {
+    width: 100,
+    backgroundColor: "grey",
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 10,
+    borderWidth: 1
   }
 });
