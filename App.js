2664984cb5483b05619e26b871763e1d57d1baaa
FreeKodak <daniel_orduz19@outlook.com>
2019-11-18 23:56:13 -0700

    Navigation Figured Out
    LETS GOOOO.
    
    For real tho if you're addign a new screen first add it to App.js then create it under the screens folder and import it into App.js

--- App.js
+++ App.js
@@ -1,88 +1,16 @@
 "use strict";
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import { Button } from "react-native-elements";
-import PresentationalComponent from "./components/PresentationalComponent";
-import Inputs from "./screens/LoginScreen";
-import { StackNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import HomeScreen from "./HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 
-export default class Home extends React.Component {
-  state = {
-    myState: "Home Page"
-  };
-
-  updateState = () => {
-    this.setState({ myState: "Updated" });
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.yellowbox}>
-          <Image source={require("./branding/logo.png")} />
-        </View>
-        <View style={styles.greybox}>
-          <TouchableOpacity style={styles.loginButton}>
-            <Text>Login</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.registerButton}>
-            <Text>Register</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "column",
-    backgroundColor: "#FCBA03",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-
-  yellowbox: {
-    height: 300,
-    width: 500,
-    backgroundColor: "grey",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-
-  greybox: {
-    height: 500,
-    width: 500,
-    backgroundColor: "#FCBA03",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-
-  loginButton: {
-    width: 100,
-    backgroundColor: "grey",
-    marginRight: 40,
-    marginLeft: 40,
-    marginTop: 10,
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 25,
-    paddingRight: 25,
-    borderRadius: 10,
-    borderWidth: 1
-  },
-
-  registerButton: {
-    width: 100,
-    backgroundColor: "grey",
-    marginRight: 40,
-    marginLeft: 40,
-    marginTop: 10,
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 15,
-    paddingRight: 15,
-    borderRadius: 10,
-    borderWidth: 1
-  }
+const MainNavigatior = createStackNavigator({
+  Home: { screen: HomeScreen },
+  Login: { screen: LoginScreen }
 });
+
+const App = createAppContainer(MainNavigatior);
+
+export default App;
